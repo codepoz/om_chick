c28a58f65071edd18dd90dbdac4cf9eeb8ce5103
alanlengkoan <alansaputralengkoan@gmail.com>
2022-12-12 09:19:57 +0800

    update again

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,8 +1,10 @@
 <template>
-    <div class="grid grid-cols-4 gap-4">
-        <div>01</div>
-        <div>09</div>
-        <div>09</div>
-        <div>09</div>
-    </div>
+    <section class="pt-10 pb-10 bg-white">
+        <div class="grid justify-items-center grid-cols-4 gap-4">
+            <div>01</div>
+            <div>09</div>
+            <div>09</div>
+            <div>09</div>
+        </div>
+    </section>
 </template>
--- src/partials/Footer.vue
+++ src/partials/Footer.vue
@@ -1,12 +1,15 @@
 <template>
-    <div class="grid grid-cols-2 gap-4">
-        <div>01</div>
-        <!-- ... -->
-        <div>
-            <h2 class="text-center">Hubungi Kami</h2>
-            <p>
-                Untuk About
-            </p>
+    <section class="pt-10 pb-10 bg-white">
+        <div class="grid grid-cols-2 gap-4">
+            <div class="pt-5 pb-5">
+                <img src="./../assets/images/logo.png" alt="">
+            </div>
+            <div class="pt-5 pb-5">
+                <h2 class="text-center">Hubungi Kami</h2>
+                <p>
+                    Untuk About
+                </p>
+            </div>
         </div>
-    </div>
+    </section>
 </template>
